1daa1104ca9ccf7dcdcb267a33ec3316f9021a57
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-12 13:11:14 +0000

    Update index.js

--- src/crypto/index.js
+++ src/crypto/index.js
@@ -1,3 +1,5 @@
 export { canonicalize } from "./canonical.js";
 export { sha256Bytes, sha256Hex } from "./sha256.js";
 export { createSigningPayload, signDemo, verifyDemo } from "./signed.js";
+export { generateEd25519KeyPair, signEd25519, verifyEd25519 } from "./ed25519.js";
+export { sign, verify } from "./signed-ed25519.js";
