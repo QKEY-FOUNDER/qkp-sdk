1b2ecdc47575e3adf27249164e02542d16e41405
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-12 10:05:14 +0000

    Create conformance-c4.js

---
+++ examples/conformance-c4.js
@@ -0,0 +1,12 @@
+import { intent, alignment } from "../src/index.js";
+
+const i = intent.createIntent({
+  issuer: "did:qkp:alice",
+  purpose: "authorize_data_access",
+});
+
+const actionOk = { purpose: "authorize_data_access" };
+const actionBad = { purpose: "something_else" };
+
+console.log("C4 aligned (should be true):", alignment.checkAlignment({ intent: i, action: actionOk }));
+console.log("C4 aligned (should be false):", alignment.checkAlignment({ intent: i, action: actionBad }));
