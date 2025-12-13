ecd4ec9c129ef23469da37d990ca7ed6349ce75c
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-13 12:20:09 +0000

    Update index.js

--- src/agents/index.js
+++ src/agents/index.js
@@ -1,3 +1,7 @@
 export { createExecutionContract } from "./contract.js";
 export { verifyExecutionContract } from "./verify.js";
 export { executeContractStub } from "./execute.js";
+export { createExecutionReceipt } from "./receipt.js";
+export { createSignedExecutionReceipt, verifySignedExecutionReceipt } from "./signed-receipt.js";
+export { createReplayGuard } from "./replay.js";
+export { auditedExecute } from "./audited-execute.js";
