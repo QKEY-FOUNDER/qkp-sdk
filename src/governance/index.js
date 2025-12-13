1bd7a26a38fa820fe38f040739a5fc460cb4c0a4
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-13 11:07:25 +0000

    Create index.js

---
+++ src/governance/index.js
@@ -0,0 +1,7 @@
+export { createProposal, isProposal } from "./proposal.js";
+export { createSignedProposal, verifySignedProposal } from "./signed-proposal.js";
+
+export { createVote, isVote } from "./vote.js";
+export { createSignedVote, verifySignedVote } from "./signed-vote.js";
+
+export { tallyVotes } from "./tally.js";
