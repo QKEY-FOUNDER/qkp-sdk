be0e650195db2152915b48651fda3b82c624218b
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-13 09:12:17 +0000

    Update index.js

--- src/claims/index.js
+++ src/claims/index.js
@@ -1,2 +1,5 @@
 export { createClaim, isClaim, isExpired } from "./claim.js";
 export { createSignedClaim, verifySignedClaim } from "./signed-claim.js";
+export { createRevocation } from "./revocation.js";
+export { createSignedRevocation, verifySignedRevocation } from "./signed-revocation.js";
+export { verifyClaimWithPolicy } from "./verify-claim.js";
