86db4f248df3a8990064a597f894eeec2cf3b438
Mihail Chiosa <mnvpchiosa@gmail.com>
2025-12-14 21:57:11 +0000

    Update index.js

--- src/graph/index.js
+++ src/graph/index.js
@@ -1,3 +1,5 @@
 export { makeNodeRef } from "./node.js";
 export { createEdge } from "./edge.js";
 export { validateNodeRef, validateEdge } from "./validate.js";
+export { createChainLink } from "./chain.js";
+export { signChainLink, verifySignedChainLink } from "./signed-chain.js";
